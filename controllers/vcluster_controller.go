155d3b102147c0ec2aa3c8bcfb8b41e3219813bc
Justin Bronn <justin.bronn@maxar.com>
2024-01-31 14:40:34 -0700

    Max `k3s` supports is 1.28.

--- controllers/vcluster_controller.go
+++ controllers/vcluster_controller.go
@@ -255,7 +255,7 @@ func (r *VClusterReconciler) redeployIfNeeded(ctx context.Context, vCluster *v1a
 
 	kVersion := vclusterhelm.Version{
 		Major: "1",
-		Minor: "29",
+		Minor: "28",
 	}
 	if vCluster.Spec.KubernetesVersion != nil && *vCluster.Spec.KubernetesVersion != "" {
 		v := strings.Split(*vCluster.Spec.KubernetesVersion, ".")
